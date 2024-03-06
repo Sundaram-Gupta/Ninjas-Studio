7b4307aead566701569d81cc2a032cf96bf8f5e3
Sundaram Gupta <160701494+Sundaram-Gupta@users.noreply.github.com>
2024-03-06 14:16:12 +0530

    i am added images , html, css and javascript files

---
+++ script.js
@@ -0,0 +1,52 @@
+// for slider-caraousel
+// setInterval(active, 2000);
+// function active() {
+//     let slider = document.querySelectorAll('.slider-img-container');
+//     sliding(slider);
+//     function sliding(slider) {
+//         if (slider[0].style.opacity == "0") {
+//             // slider[0].style.opacity = "1";
+//             slider[1].style.opacity = "1";
+//             slider[2].style.opacity = "0";
+//         }
+//         // else {
+//         //     // if (i == 0) {
+
+//         //     // }
+//         // }
+//     }
+//     console.log(slider[0]);
+// }
+
+
+
+// // For responsive-navbar
+// function navbar() {
+//     if (document.style.max - width <= "768px") {
+//         // document.getElementById('navbar').style.display = "flex";
+//     }
+// }
+
+
+// // [7, 5, 11, 9, 13]
+
+// const arr = [7,5,11,9,13];
+
+// let minn = 100;
+// let min = minn;
+// let k=3, index=-1;
+// kthSmallest(arr);
+// function kthSmallest(arr){
+//     while(k>0){
+//         for(let i=0; i<arr.length; i++){
+//             if(arr[i] < minn && arr[i] < min){
+//                 min = arr[i];
+//                 index = i;
+//             }
+//         }
+//         console.log(min);
+//         arr[index] = 200;
+//         min = 100;
+//         k--;
+//     }
+// }
